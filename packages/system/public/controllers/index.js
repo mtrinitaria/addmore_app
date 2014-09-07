8378f7ebb2a3595814136ba305b7572ac81e71f6
mykhel trinitaria <mykhel0003@yahoo.com>
2014-09-08 04:57:38 +0800

    jumbotron

--- packages/system/public/controllers/index.js
+++ packages/system/public/controllers/index.js
@@ -3,5 +3,12 @@
 angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
   function($scope, Global) {
     $scope.global = Global;
+    
+    
+    setTimeout(function(){
+      $scope.welcomeUser = Global.user.name;
+      console.log($scope.welcomeUser,Global.user.name);
+      $scope.$apply();
+    }, 100);
   }
 ]);
